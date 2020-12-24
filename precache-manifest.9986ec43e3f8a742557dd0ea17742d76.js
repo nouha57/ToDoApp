ab38fcd9c063f8faf7adc9a977dbea3193f3425e
nouha z <60858818+nouha57@users.noreply.github.com>
2020-12-24 10:29:02 +0100

    Updates

---
+++ precache-manifest.9986ec43e3f8a742557dd0ea17742d76.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7991feeb53c56485ee65085e94e584f",
+    "url": "/ToDoApp/index.html"
+  },
+  {
+    "revision": "d1cad4a480593a9fd084",
+    "url": "/ToDoApp/static/css/main.e7714be9.chunk.css"
+  },
+  {
+    "revision": "3f37cb9ccd3359fc2ab3",
+    "url": "/ToDoApp/static/js/2.0b4b5c19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoApp/static/js/2.0b4b5c19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1cad4a480593a9fd084",
+    "url": "/ToDoApp/static/js/main.b26c3e53.chunk.js"
+  },
+  {
+    "revision": "4c4904a65349851007e9",
+    "url": "/ToDoApp/static/js/runtime-main.9c10cc15.js"
+  }
+]);
